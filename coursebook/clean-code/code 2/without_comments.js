b3944a08deff417dfbd359fd69a0233f65cc29ee
joel-shor <joelshor@google.com>
2018-08-14 12:39:36 +0300

    Fix code 2 code.

--- coursebook/clean-code/code 2/without_comments.js
+++ coursebook/clean-code/code 2/without_comments.js
@@ -1,20 +1,21 @@
-function func(arr, compare_Function) {
+function main_func(arr, compare_Function) {
 
-  function tmp(a, b) {
+  function compare(a, b) {
    return a - b;
    }
   var min = 0;
   var index = 0;
   var temp = 0;
 
-  do_stuff = do_stuff || tmp;
+ //{Function} compare_Function Compare function
+  compare_Function = compare_Function || compare;
 
   for (var i = 0; i < arr.length; i += 1) {
     index = i;
     min = arr[i];
 
     for (var j = i + 1; j < arr.length; j += 1) {
-      if (do_stuff(min, arr[j]) > 0) {
+      if (compare_Function(min, arr[j]) > 0) {
         min = arr[j];
         index = j;
       }
@@ -25,8 +26,9 @@ function func(arr, compare_Function) {
     arr[index] = temp;
   }
 
+  //return sorted arr
   return arr;
 }
 
-console.log(func([3, 0, 2, 5, -1, 4, 1], func(a, b) { return a - b; }));
-console.log(func([3, 0, 2, 5, -1, 4, 1], func(a, b) { return b - a; }));
+console.log(main_func([3, 0, 2, 5, -1, 4, 1], function(a, b) { return a - b; }));
+console.log(main_func([3, 0, 2, 5, -1, 4, 1], function(a, b) { return b - a; }));
